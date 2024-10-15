ad2cd0587773579976764e9c71e938b8bab1e4dd
andrejstrogonov <strogonovandrej@hotmail.com>
2024-10-15 22:33:14 +0300

    First part of homework

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,48 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //задача 1
+        for  (int i=1;i <= 10; i++){
+            System.out.println(i);
+        }
+
+        //задача 2
+        for  (int i1=10; i1 > 0; i1--){
+            System.out.println(i1);
+        }
+
+
+        //задача 3
+
+        for  (int i=1;i <= 17; i++) {
+            if ((i % 2 == 0)) {
+                System.out.println(i);
+            }
+        }
+
+        //задача 4
+        for( int i = 10 ; i >= -10; i--){
+            if (i>0){
+                System.out.println(i);
+            }
+            if(i<=0){
+                System.out.println(i);
+            }
+
+        }
+        //задача 5
+        for (int i = 1904; i < 2096; i=i+4){
+            System.out.println(i+" год является високосным");
+        }
+
+        //задача 6
+
+        //задача 7
+
+        //задача 8
+
+        //задчас 9
+
+        //задача 10
+
     }
 }
